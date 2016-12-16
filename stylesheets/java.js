0c6efa4e19a5c1e8252477723d52e9484aab4ca3
dscace <derek.scace@gmail.com>
2016-12-16 11:53:27 -0700

    added back to top

--- stylesheets/java.js
+++ stylesheets/java.js
@@ -3,4 +3,29 @@ $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({full_width: true});
 });
 
+$("#button1").click(function(){
+  $('html, body').animate({
+      scrollTop: $("#projects").offset().top
+  }, 1200);
+});
 
+$("#button2").click(function(){
+  $('html, body').animate({
+      scrollTop: $("#education").offset().top
+  }, 1200);
+});
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > 300 ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
